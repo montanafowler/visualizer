896363ed82fd539d421f61fd9bf71f5dbc5dbf57
montanafowler <montanafowler@yahoo.com>
2021-09-18 10:31:37 -0700

    fixed the primitive buttons from crashing at initial deactivation, by making an ObjectActivationManager instead of the three primitives all starting the same script:

--- visualizer/Assets/Scripts/ObjectLibraryButtonHandler.cs
+++ visualizer/Assets/Scripts/ObjectLibraryButtonHandler.cs
@@ -1,33 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ObjectLibraryButtonHandler : MonoBehaviour
 {
 	private GameObject cube;
 	private GameObject cylinder;
+	private Vector3 cylinderScale;
 	private GameObject sphere;
+	private Vector3 sphereScale;
+	private bool started = false;
 
 	void Start() {
-		cube = GameObject.Find("CubeObject");//.GetComponent<MoveObject>();
-		cylinder = GameObject.Find("CylinderObject");//.GetComponent<Cylinder>();
-		sphere = GameObject.Find("SphereObject");//.GetComponent<Sphere>();
-		cylinder.SetActive(false);
-  //  	sphere.SetActive(false);
+		// save references to the basic primitives
+		cube = GameObject.Find("CubeObject");
+		cylinder = GameObject.Find("CylinderObject");
+		sphere = GameObject.Find("SphereObject");
+
+		// deactivate the cylinder and sphere if they aren't already deactivated
+		if(cylinder)
+			cylinder.SetActive(false);
+		if(sphere)
+			sphere.SetActive(false);
 	}
 
+	/** enable the cube primitive */
     public void enableCube() {
     	cube.SetActive(true);
     	cylinder.SetActive(false);
     	sphere.SetActive(false);
     }
 
+    /** enable the cylinder primitive */
     public void enableCylinder() {
-    	cube.SetActive(false);
-    	cylinder.SetActive(true);
-    	sphere.SetActive(false);
+    		cube.SetActive(false);
+    		cylinder.SetActive(true);
+    		sphere.SetActive(false);
     }
 
+    /** enable the sphere primitive */
     public void enableSphere() {
     	cube.SetActive(false);
     	cylinder.SetActive(false);
