451b7bc4aabb1d4a5c7b3f889148b7f71679a26f
montanafowler <montanafowler@yahoo.com>
2021-09-19 16:24:50 -0700

    made the material buttons & materials, now I need to make the buttons add the material to the active model

---
+++ visualizer/Assets/Scripts/RotateObject.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RotateObject : MonoBehaviour
+{
+    /** on slider changed, scale the active 3d model */
+    public void sliderChanged() 
+    {
+    	Slider xSlider = GameObject.Find("XRotateSlider").GetComponent<Slider>();
+    	Slider ySlider = GameObject.Find("YRotateSlider").GetComponent<Slider>();
+    	Slider zSlider = GameObject.Find("ZRotateSlider").GetComponent<Slider>();  
+
+    	GameObject cube = GameObject.Find("CubeObject");
+    	GameObject sphere = GameObject.Find("SphereObject");
+    	GameObject cylinder = GameObject.Find("CylinderObject");
+    	
+    	// if the cube is active, rotate it
+    	if(cube)
+    		cube.transform.Rotate(xSlider.value, ySlider.value, zSlider.value, Space.Self);
+    	
+    	// if the sphere is active, rotate it
+    	if(sphere)
+    		sphere.transform.Rotate(xSlider.value, ySlider.value, zSlider.value, Space.Self);
+
+    	// if the cylinder is active, rotate it
+    	if(cylinder)
+    		cylinder.transform.Rotate(xSlider.value, ySlider.value, zSlider.value, Space.Self);
+    }
+}
