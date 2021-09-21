31aeaf12df5866df6d9c6d2a1eed3a65f9f31c7e
montanafowler <montanafowler@yahoo.com>
2021-09-20 17:53:40 -0700

    did compressions of textures

--- visualizer/Assets/Scripts/ObjectLibraryButtonHandler.cs
+++ visualizer/Assets/Scripts/ObjectLibraryButtonHandler.cs
@@ -5,42 +5,27 @@ using UnityEngine.UI;
 
 public class ObjectLibraryButtonHandler : MonoBehaviour
 {
-	private GameObject cube;
-	private GameObject cylinder;
-	private GameObject sphere;
+	private Dictionary<string, GameObject> shapes = new Dictionary<string, GameObject>();
 
-	void Start() {
+	void Start() 
+	{
 		// save references to the basic primitives
-		cube = GameObject.Find("CubeObject");
-		cylinder = GameObject.Find("CylinderObject");
-		sphere = GameObject.Find("SphereObject");
+		shapes["CUBE"] = GameObject.Find("CubeObject");
+		shapes["CYLINDER"] = GameObject.Find("CylinderObject");
+		shapes["SPHERE"] = GameObject.Find("SphereObject");
 
-		// deactivate the cylinder and sphere if they aren't already deactivated
-		if(cylinder)
-			cylinder.SetActive(false);
-		if(sphere)
-			sphere.SetActive(false);
+		activateShape("CUBE"); // activate the cube first
 	}
 
-	/** enable the cube primitive */
-    public void enableCube() {
-    	cube.SetActive(true);
-    	cylinder.SetActive(false);
-    	sphere.SetActive(false);
-    }
-
-    /** enable the cylinder primitive */
-    public void enableCylinder() {
-    		cube.SetActive(false);
-    		cylinder.SetActive(true);
-    		sphere.SetActive(false);
-    }
-
-    /** enable the sphere primitive */
-    public void enableSphere() {
-    	cube.SetActive(false);
-    	cylinder.SetActive(false);
-    	sphere.SetActive(true);
-    }
-    
+	/** activate the shape type */
+	public void activateShape(string shapeType)
+	{
+		foreach(KeyValuePair<string, GameObject> shape in shapes)
+		{
+		    if(shape.Key == shapeType)
+		    	shape.Value.SetActive(true);
+		    else
+		    	shape.Value.SetActive(false);
+		}
+	}
 }
